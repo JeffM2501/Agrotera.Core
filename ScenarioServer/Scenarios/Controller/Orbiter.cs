a887898b0d3ae93fd8ff67b50c0ff6e77f951762
JeffM2501 <JeffM2501@gmail.com>
2016-12-20 20:48:45 -0800

    xx

---
+++ ScenarioServer/Scenarios/Controller/Orbiter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Entities;
+
+namespace ScenarioServer.Scenarios.Controller
+{
+	public class Orbiter : IEntityContorller
+	{
+		public Orbiter(Entity parrent, double speed)
+		{
+
+		}
+
+		void IEntityContorller.Init(Entity ent)
+		{
+			
+		}
+
+		void IEntityContorller.Update(Entity ent, double delta)
+		{
+			
+		}
+	}
+}
