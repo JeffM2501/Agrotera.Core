5178c8485c29b6ce38afcff3e7f537de38f2089c
Jeffrey Myers <JeffM2501@Gmail.com>
2017-01-05 12:46:55 -0800

    add client validator to check simulation positions

---
+++ Tools/ClientValidator/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ClientValidator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.StatusText = new System.Windows.Forms.TextBox();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Map = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Map)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.StatusText);
+            this.splitContainer1.Panel1.Controls.Add(this.ConnectButton);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.Map);
+            this.splitContainer1.Size = new System.Drawing.Size(824, 494);
+            this.splitContainer1.SplitterDistance = 170;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // StatusText
+            // 
+            this.StatusText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StatusText.Location = new System.Drawing.Point(13, 42);
+            this.StatusText.Multiline = true;
+            this.StatusText.Name = "StatusText";
+            this.StatusText.ReadOnly = true;
+            this.StatusText.Size = new System.Drawing.Size(154, 73);
+            this.StatusText.TabIndex = 1;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(12, 12);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(75, 23);
+            this.ConnectButton.TabIndex = 0;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Map
+            // 
+            this.Map.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Map.Location = new System.Drawing.Point(0, 0);
+            this.Map.Name = "Map";
+            this.Map.Size = new System.Drawing.Size(650, 494);
+            this.Map.TabIndex = 0;
+            this.Map.TabStop = false;
+            this.Map.Paint += new System.Windows.Forms.PaintEventHandler(this.Map_Paint);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "SS1.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 494);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Map)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.TextBox StatusText;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox Map;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ Tools/ClientValidator/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using ShipClient;
+
+namespace ClientValidator
+{
+    public partial class Form1 : Form
+    {
+        ShipConnection Connection = null;
+
+        public class GraphicInfo
+        {
+            public Brush Color = Brushes.Magenta;
+            public Image Pic = null;
+            public float Size = 5;
+
+            public GraphicInfo(Brush c, float s)
+            {
+                Color = c;
+                Size = s;
+            }
+
+            public GraphicInfo(Image p, float s)
+            {
+                Color = Brushes.White;
+                Pic = p;
+                Size = s;
+            }
+        }
+
+        public Dictionary<string, GraphicInfo> MapElementColors = new Dictionary<string, GraphicInfo>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+
+            //    Application.Idle += Application_Idle;
+
+            MapElementColors.Add("Station", new GraphicInfo(imageList1.Images[0],32));
+            MapElementColors.Add("CargoStack", new GraphicInfo(Brushes.Green,10));
+            MapElementColors.Add("Shuttle", new GraphicInfo(Brushes.Blue,5));
+            MapElementColors.Add("PlayerShip", new GraphicInfo(Brushes.White,8));
+        }
+
+        private void Application_Idle(object sender, EventArgs e)
+        {
+            if (Connection != null)
+                Connection.Update();
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            Connection = new ShipConnection("localhost", 2501);
+            Connection.Connected += Connection_Connected;
+            Connection.Disconnected += Connection_Disconnected;
+            Connection.ShipAssigned += Connection_ShipAssigned;
+        }
+
+        private void Connection_ShipAssigned(object sender, EventArgs e)
+        {
+            StatusText.Text = "Ship Assigned";
+            Map.Invalidate();
+            Connection.PlayerShip.SensorEntityAppeared += PlayerShip_SensorEntityAppeared;
+            Connection.PlayerShip.SensorEntityRemoved += PlayerShip_SensorEntityRemoved;
+            Connection.PlayerShip.SensorEntityUpdated += PlayerShip_SensorEntityUpdated;
+        }
+
+        private void PlayerShip_SensorEntityUpdated(object sender, Entities.Classes.Ship.KnownEntity e)
+        {
+            Map.Invalidate();
+          //  throw new NotImplementedException();
+        }
+
+        private void PlayerShip_SensorEntityRemoved(object sender, Entities.Classes.Ship.KnownEntity e)
+        {
+            Map.Invalidate();
+            // throw new NotImplementedException();
+        }
+
+        private void PlayerShip_SensorEntityAppeared(object sender, Entities.Classes.Ship.KnownEntity e)
+        {
+            Map.Invalidate();
+            //  throw new NotImplementedException();
+        }
+
+        private void Connection_Disconnected(object sender, EventArgs e)
+        {
+            StatusText.Text = "Disconnected";
+        }
+
+        private void Connection_Connected(object sender, ShipConnection.ConnectedEventArgs e)
+        {
+            StatusText.Text = "Connected";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Connection != null)
+                Connection.Update();
+        }
+
+        private void Map_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.Black);
+
+            if (Connection == null)
+                return;
+
+            if (Connection.PlayerShip == null)
+                return;
+
+            e.Graphics.TranslateTransform(Map.Width * 0.5f, Map.Height * 0.5f);
+
+            double scale = 0.25;
+
+           
+
+            foreach(var i in Connection.PlayerShip.KnownEntities.Values)
+            {
+                RectangleF rect = new RectangleF();
+
+
+                float size = 5;
+                Brush b = Brushes.LightYellow;
+                Image pic = null;
+
+                if (MapElementColors.ContainsKey(i.BaseEntity.VisualGraphics))
+                {
+                    var d = MapElementColors[i.BaseEntity.VisualGraphics];
+                    if (d.Pic == null)
+                        b = d.Color;
+                    else
+                        pic = d.Pic;
+
+                    size = d.Size;
+                }
+   
+                rect.X = (float)(i.LastPosition.X * scale);
+                rect.Y = (float)(i.LastPosition.Y * scale);
+
+                rect.X -= size;
+                rect.Y -= size;
+
+                rect.Width = size;
+                rect.Height = size;
+
+                if (pic != null)
+                    e.Graphics.DrawImage(pic, rect);
+                else
+                    e.Graphics.FillRectangle(b, rect);
+            }
+        }
+    }
+}
